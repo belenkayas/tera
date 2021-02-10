56a90bd0204f670936dae1969ed19114ecf3689e
belenkayas <55450454+belenkayas@users.noreply.github.com>
2021-02-10 18:15:34 +0300

    new commit (Беленькая)

--- like_button.js
+++ like_button.js
@@ -1,34 +1,34 @@
-'use strict';
+"use strict";
 
 const e = React.createElement;
 
 class LikeButton extends React.Component {
-constructor(props) {
-super(props);
-this.state = { liked: false };
+	constructor(props){
+		super(props);
+		this.state = {liked: false};
+	}
+	
+	render(){
+		if (this.state.liked){
+			return 'You liked this.';
+		}
+		
+		return e(
+		'button',
+		{onClick: () => this.setState({liked:true})},
+		'Like'
+		);
+		
+	}
 }
 
-render() {
-if (this.state.liked) {
-return 'You liked comment number ' + this.props.commentID;
-}
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
 
-return e(
-'button',
-{ onClick: () => this.setState({ liked: true }) },
-'Like'
-);
-}
-}
-
-// const domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(e(LikeButton), domContainer);
-
-document.querySelectorAll('.like_button_container')
-.forEach(domContainer => {
-const commentID = parseInt(domContainer.dataset.commentid, 10);
-ReactDOM.render(
-e(LikeButton, { commentID: commentID}),
-domContainer
-);
+document.querySelectorAll('.like_button_container').forEach(domContainer => {
+	const commentID = parseInt(domContainer.dataset.commentID,10);
+	ReactDOM.render(
+	e(LikeButton, {commentID: commentID}),
+	domContainer
+	);
 });
